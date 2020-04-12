d1832266a20e116ee1985321eb9ec98637252eb1
Yanters <43695512+Yanters@users.noreply.github.com>
2020-04-12 16:45:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,60 +19,19 @@ bot.on('message', message => {
     switch (args[0]) {
         case 'not':
             var p="",z="",o="",l="",u="",k="";
-
-            for(var i=0; i<args.length;i++)
+            var test = "";
+            for(var i=0; i<args.length();i++)
             {
-                if(args[i]="P:")
-                {
-                    if(args[i]!="Z:")
-                    {
-                        p= p + args[i+1];
-                    }
-                }
-                if(args[i]="Z:")
-                {
-                    if(args[i]!="O:")
-                    {
-                        z= z + args[i+1];
-                    }
-                }
-                if(args[i]="O:")
-                {
-                    if(args[i]!="L:")
-                    {
-                        o= o + args[i+1];
-                    }
-                }
-                if(args[i]="L:")
-                {
-                    if(args[i]!="U:")
-                    {
-                        l= l + args[i+1];
-                    }
-                }
-                if(args[i]="U:")
-                {
-                    if(args[i]!="K:")
-                    {
-                        u= u + args[i+1];
-                    }
-                }
-                if(args[i]="K:")
-                {
-                    if(i<args.length)
-                    {
-                        k= k + args[i+1];
-                    }
-                }
+                test += test + " ";
             }
-            message.channel.sendMessage(args[1]);
-            message.channel.sendMessage(p);
+            message.channel.sendMessage(test);
+           /* message.channel.sendMessage(p);
             message.channel.sendMessage(z);
             message.channel.sendMessage(o);
             message.channel.sendMessage(l);
             message.channel.sendMessage(u);
             message.channel.sendMessage(k);
-            /*var exampleEmbed = new Discord.RichEmbed()
+            var exampleEmbed = new Discord.RichEmbed()
             .setColor('#A40000')
             .setTitle('Typ akcji:  '+ p)
             .setDescription(` ** Zgłaszający: ** ${z} \n ** Opis zdarzenia: **  ${o}  `)
